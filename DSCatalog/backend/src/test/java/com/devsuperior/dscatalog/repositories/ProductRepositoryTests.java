45106ee84688bdaff88c874404ddc3b0f114569c
MatheusDario <dario.matheus97@gmail.com>
2022-05-25 13:22:42 -0300

    Class concluid tests repository

--- DSCatalog/backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ DSCatalog/backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,11 +3,14 @@ package com.devsuperior.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -15,13 +18,48 @@ public class ProductRepositoryTests {
 	@Autowired  
 	private ProductRepository repository;
 	
+	private long exintingId;
+	long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		exintingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test 
+	public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+		Product product = Factory.creatProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+		
+	}
+	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		long exintingId = 1L;
-		
 		repository.deleteById(exintingId);
 		
 		Optional<Product> result = repository.findById(exintingId);
 		Assertions.assertFalse(result.isPresent());
 	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+	}
+	
+	
+	
+	
+	
+	
+	
 }
